285631aaa20baafcc09c0c6cf1260eed13b67884
rajib5570 <rajib5570@gmail.com>
2024-08-16 01:54:50 +0600

    Mainlayout.tsx modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav>
-        <div>
+        <div className='text-green-600'>
             BD<span className='text-red-600'>IT</span>
         </div>
         <div></div>
