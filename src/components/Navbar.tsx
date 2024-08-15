cf58905e8b72e03ddd0893a448542e076fc50f7d
rajib5570 <rajib5570@gmail.com>
2024-08-16 03:32:25 +0600

    Navbar.tsx modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar: React.FC = () => {
                 </div>
                 {isOpen && 
                     (
-                        <div className='origin-top-right'>
+                        <div className='origin-top-right absolute'>
                         </div>
                     )
                 }
