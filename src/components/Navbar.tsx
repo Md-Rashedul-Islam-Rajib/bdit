cdff235ba88619703d742717f2c738922a612914
rajib5570 <rajib5570@gmail.com>
2024-08-16 02:32:35 +0600

    Mainlayout.tsx modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const user = true;
   return (
     <nav>
         <div className='text-green-600 text-4xl font-bold bg-black py-4 px-2 cursor-pointer'>
            <Link to='/'> BD<span className='text-red-600'>IT</span> </Link>
         </div>
-        <div></div>
-        <div></div>
+        <div>
+        </div>
+        <div>
+            {
+                user ? :
+            }
+        </div>
     </nav>
   )
 }
