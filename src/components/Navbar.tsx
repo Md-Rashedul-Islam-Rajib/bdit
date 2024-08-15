12ab046fd11d93ee9d2b08897bd9fcdc9e4feb7c
rajib5570 <rajib5570@gmail.com>
2024-08-16 02:28:02 +0600

    Mainlayout.tsx modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
-        <div className='text-green-600 text-4xl font-bold bg-black py-4 px-2'>
+        <div className='text-green-600 text-4xl font-bold bg-black py-4 px-2 cursor-pointer'>
            <Link to='/'> BD<span className='text-red-600'>IT</span> </Link>
         </div>
         <div></div>
