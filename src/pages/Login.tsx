9dced16a537047ba15b0682dc44156fb8b4635c8
rajib5570 <rajib5570@gmail.com>
2024-08-16 23:54:43 +0600

    Login.tsx modified

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaEye, FaEyeSlash, FaGithub } from "react-icons/fa";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const Login = () => {
   return (
