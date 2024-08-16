513dc3d583f005b4126a6e6b19a39ee2f5e8343e
rajib5570 <rajib5570@gmail.com>
2024-08-16 18:36:44 +0600

    Footer.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
 
       </div>
 
-      <div className="px-4">
+      <div className="px-4 flex">
 
         <div className="text-green-600 text-2xl md:text-3xl lg:text-4xl font-bold cursor-pointer">
          <Link to='/'> BD<span className="text-red-600">IT</span></Link>
