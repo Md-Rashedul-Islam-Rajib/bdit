12072eec0643fe6ab99f7ac3edd9d03e31ceee8b
rajib5570 <rajib5570@gmail.com>
2024-08-16 21:14:56 +0600

    Card.tsx modified

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,7 +17,7 @@ const Card = () => {
       <div className='size-36 md:size-48 lg:size-60 cursor-pointer hover:scale-105'>
         <img src={demo.image} alt="Product" />
       </div>
-        <h3 className='md:text-lg lg:text-xl font-semibold text-red-600'>{demo.name}</h3>
+        <h3 className='md:text-lg lg:text-xl font-semibold text-red-600 text-center'>{demo.name}</h3>
         <p>{demo.price}৳</p>
     </div>
   )
