3c971f189ed781c281ec08d7ab2be31d6fa3040e
rajib5570 <rajib5570@gmail.com>
2024-08-16 21:44:56 +0600

    main.tsx modified

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes'
 import { QueryClient } from '@tanstack/react-query'
 
-
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
