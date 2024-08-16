87006661bd0216fd0a551f7f4234385ed3285e6d
rajib5570 <rajib5570@gmail.com>
2024-08-17 02:16:49 +0600

    Authprovider.tsx added

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,10 @@
+import {
+    GithubAuthProvider,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    User,
+  } from "firebase/auth";
