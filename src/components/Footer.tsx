3d6c0b616a850a494b1cc778ad4eef47db44101d
rajib5570 <rajib5570@gmail.com>
2024-08-16 18:33:49 +0600

    Footer.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
           <h2> About Us</h2>
           <p>Contact Us</p>
           <p>Warranty Policy</p>
-          <p>Warranty Policy</p>
+          <p>Terms and Conditions</p>
         </div>
 
       </div>
