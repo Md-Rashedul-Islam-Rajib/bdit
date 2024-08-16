c7a4952e53a3820d336d9f4bd1f236cdca1d83fc
rajib5570 <rajib5570@gmail.com>
2024-08-17 01:22:02 +0600

    Login.tsx modified

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -127,6 +127,7 @@ const Login: React.FC = () => {
           <div className="flex justify-between my-4 font-semibold">
           <p>New here?</p>
           <Link to='/register'>
+          <p></p>
           </Link>
           </div>
 
