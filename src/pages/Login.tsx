c036117eae7a660be886c099edd405743866860a
rajib5570 <rajib5570@gmail.com>
2024-08-16 23:54:01 +0600

    Login.tsx modified

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+
 const Login = () => {
   return (
     
