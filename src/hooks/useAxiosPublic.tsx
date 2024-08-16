f348fb97f468cd8a32e40438bed9b1a163d8fc5f
rajib5570 <rajib5570@gmail.com>
2024-08-16 21:52:14 +0600

    useAxiosPublic.tsx modified

--- src/hooks/useAxiosPublic.tsx
+++ src/hooks/useAxiosPublic.tsx
@@ -1,3 +1,9 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+
+const axiosPublic : AxiosInstance = axios.create(
+    {
+        baseURL: "http://localhost:5000"
+    }
+);
 
 export default useAxiosPublic
