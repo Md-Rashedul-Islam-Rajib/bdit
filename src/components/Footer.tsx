d3e7e962a43f7a2fcc43ef3f6ba2f824f48451c7
rajib5570 <rajib5570@gmail.com>
2024-08-16 17:52:22 +0600

    Footer.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,19 @@ const Footer = () => {
   return (
     <footer className='bg-black text-white py-4'>
       <div>
+        
         <div className='bg-sky-400 size-8 p-2 rounded-full'>
         <FaFacebookF />
         </div>
+        
         <div className="bg-sky-400 size-8 p-2 rounded-full">
         <FaInstagram />
         </div>
+
+        <div>
+        
+        </div>
+
       </div>
       <p className='text-center'>Copyright © 2024, BDIT, All Rights Reserved</p>
     </footer>
