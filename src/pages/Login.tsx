6d881ad8423f54dbc6e34d8b08a5e9afd3b82e19
rajib5570 <rajib5570@gmail.com>
2024-08-17 01:06:47 +0600

    Login.tsx modified

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -86,7 +86,7 @@ const Login: React.FC = () => {
           )}
         </label>
 
-          <label className="input input-bordered flex items-center">
+          <label className="input input-bordered flex items-center gap-2">
 
           </label>
       </form>
