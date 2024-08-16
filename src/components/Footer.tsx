6f3eb5d22aedbe8d7b07eaaa499bbc8e02dc04f5
rajib5570 <rajib5570@gmail.com>
2024-08-16 17:10:50 +0600

    Footer.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-black text-white'>
+    <footer className='bg-black text-white py-4'>
       <p className='text-center'>Copyright © 2024, BDIT, All Rights Reserved</p>
     </footer>
   )
