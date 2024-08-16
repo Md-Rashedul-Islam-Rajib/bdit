2287213aa3c7d3398a990fe0e590f2d2a2ba9ae4
rajib5570 <rajib5570@gmail.com>
2024-08-16 18:17:44 +0600

    Footer.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,10 +37,12 @@ const Footer = () => {
 
       </div>
 
-      <div>
+      <div className="px-4">
         <div className="text-green-600 text-2xl md:text-3xl lg:text-4xl font-bold cursor-pointer">
          <Link to='/'> BD<span className="text-red-600">IT</span></Link>
         </div>
+
+
       </div>
       
       <p className='text-center'>Copyright © 2024, BDIT, All Rights Reserved</p>
