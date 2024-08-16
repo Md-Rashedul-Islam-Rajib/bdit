3afbfbce554f60026bf70242e7a03ae8053aa069
rajib5570 <rajib5570@gmail.com>
2024-08-16 14:52:35 +0600

    Navbar.tsx modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar: React.FC = () => {
                         <button 
                         onClick={handleLogout}
                         role='menuitem'
-                        className='text-gray-700 block px-4 py-2 text-sm w-full hover:text-gray-200'
+                        className='text-gray-700 block px-4 py-2 text-sm w-full hover:text-gray-200 text-center'
                         >
                         </button>
                         </div>
