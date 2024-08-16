7b19c4f63c96181a875cc7da8f17ecc07b48003c
rajib5570 <rajib5570@gmail.com>
2024-08-16 17:43:47 +0600

    Footer.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FaFacebookF } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className='bg-black text-white py-4'>
-      <div></div>
+      <div>
+
+      </div>
       <p className='text-center'>Copyright © 2024, BDIT, All Rights Reserved</p>
     </footer>
   )
