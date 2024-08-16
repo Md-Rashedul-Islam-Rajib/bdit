f3b9a7ff8b9e63d1fc1a10f8c57617612eb92556
rajib5570 <rajib5570@gmail.com>
2024-08-16 21:49:34 +0600

    useAxiosPublic.tsx added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
 
-import { useEffect } from 'react'
 import Card from '../components/Card'
-import axios from 'axios'
+
 
 const Home = () => {
-  useEffect(()=>{
-    axios.get('/data.json')
-    .then
-  },[])
+ 
   return (
     <main>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
