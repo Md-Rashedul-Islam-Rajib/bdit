dd009969cce887e8d059f02b734dea3d0ecd0710
rajib5570 <rajib5570@gmail.com>
2024-08-16 21:14:10 +0600

    Card.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Card from '../components/Card'
 const Home = () => {
   return (
     <main>
-      <div className='grid md:grid-cols-2 lg:grid-cols-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
         <Card />
       </div>
     </main>
