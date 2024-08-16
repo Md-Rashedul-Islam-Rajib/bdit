6a82a16c88e65e5c3ccfa537de46aa5281b69c22
rajib5570 <rajib5570@gmail.com>
2024-08-16 17:42:10 +0600

    Navbar.tsx modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-black text-white py-4'>
-      
+      <div></div>
       <p className='text-center'>Copyright © 2024, BDIT, All Rights Reserved</p>
     </footer>
   )
