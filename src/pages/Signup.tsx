bc558b515ddb3da37fa1ea2e58b90b5468dafd70
rajib5570 <rajib5570@gmail.com>
2024-08-17 14:31:39 +0600

    Signup.tsx modified

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -73,7 +73,7 @@ const Signup: React.FC = () => {
           )}
           </label>
 
-          <label className="input input-bordered flex items-center">
+          <label className="input input-bordered flex items-center gap-2">
 
           </label>
 
