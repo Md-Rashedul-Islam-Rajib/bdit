32a5f9eceb39538d2dddbcd5fb19f0d3d11e07ca
rajib5570 <rajib5570@gmail.com>
2024-08-17 18:37:07 +0600

    main.tsx modified

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/Routes'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import AuthProvider from './context/AuthProvider'
-
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./context/AuthProvider";
+import { SearchProvider } from "./context/SearchProvider";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}></RouterProvider>
-    </QueryClientProvider>
-    </AuthProvider>
-  </StrictMode>,
-)
+    <SearchProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
+      </AuthProvider>
+    </SearchProvider>
+  </StrictMode>
+);
