05656e20f3f8a2b4160bfe741469d777fd5d9144
rajib5570 <rajib5570@gmail.com>
2024-08-17 17:39:52 +0600

    Pagination.tsx modified

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -23,9 +23,10 @@ const Pagination: React.FC<PaginationProps> = ({currentPage, totalPages,onPageCh
         onClick={()=> onPageChange(index+1)}
         className={`px-4 py-2 ${currentPage === index+1 ? "bg-gray-700 text-white": "bg-black text-white"}`}
         >
-
+            {index+1}
         </button>
       ))}
+      
     </div>
   )
 }
