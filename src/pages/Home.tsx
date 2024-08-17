1aab4360437e1373d731a1437cefc13ab8c6d9d8
rajib5570 <rajib5570@gmail.com>
2024-08-17 16:37:52 +0600

    Home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 
 // import Card from '../components/Card'
 
+import { useQuery } from "@tanstack/react-query"
+import useAxiosPublic from "../hooks/useAxiosPublic"
+
 
 const Home = () => {
  
+  const axiosPublic = useAxiosPublic();
+
+  const {data : products = []} = useQuery({
+    queryKey: ['all-products'],
+    queryFn: async () => {
+      const response = await axios
+    }
+  })
+
+
   return (
     <main>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
