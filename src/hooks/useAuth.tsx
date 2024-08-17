2a9eedbdcbac5968be2d7889376b27eeb22e714f
rajib5570 <rajib5570@gmail.com>
2024-08-17 13:56:24 +0600

    useAuth.tsx modified

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,6 +3,10 @@ import { AuthContext } from '../context/AuthProvider'
 
 const useAuth = () => {
   const auth = useContext(AuthContext);
+  if(!auth) {
+    throw new Error("No Context Found")
+  }
+  return auth;
 }
 
 export default useAuth
