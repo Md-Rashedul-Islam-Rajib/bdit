da25a797f43780ffa8b8099aea58f58aa8b757b8
rajib5570 <rajib5570@gmail.com>
2024-08-17 14:03:25 +0600

    Signup.tsx modified

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,6 +2,8 @@ import React, { useContext, useState } from "react";
 import { IoImagesOutline } from "react-icons/io5";
 import { AuthContext } from "../context/AuthProvider.tsx";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Signup = () => {
   return (
