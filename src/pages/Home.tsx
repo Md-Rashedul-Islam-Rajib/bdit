1aeaafb75e295ff1ae9de9b2c1e328b2380b16b5
rajib5570 <rajib5570@gmail.com>
2024-08-18 15:15:07 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -100,7 +100,7 @@ const Home: React.FC = () => {
         <option value="GPU">Graphics Processing Unit</option>
         <option value="CPU Cooler">CPU Cooler</option>
         <option value="motherboard">Motherboard</option>
-        <option value="motherboard">Motherboard</option>
+        <option value="processor">Motherboard</option>
 
       </select>
 
