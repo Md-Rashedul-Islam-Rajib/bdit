864f4282161913d7afa204812affe944c1660ed5
rajib5570 <rajib5570@gmail.com>
2024-08-18 14:57:35 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -72,7 +72,7 @@ const Home: React.FC = () => {
         <option value='intel'>Intel</option>
         <option value='MSI'>Micro Star International</option>
         <option value='maxsun'>Maxsun</option>
-        <option value='seagate'>Maxsun</option>
+        <option value='seagate'>Seagate</option>
         <option value='NZXT'>NZXT</option>
         <option value='asus'>Asus</option>
         <option value='EKWB'>EKWB</option>
