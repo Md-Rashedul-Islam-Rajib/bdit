be59b8748aaf45a3ccbb33ebd348e86aea184350
rajib5570 <rajib5570@gmail.com>
2024-08-18 15:08:18 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -93,7 +93,7 @@ const Home: React.FC = () => {
       onChange={(e) => setSelectedCategory(e.target.value)}
       >
         <option value="">All Category</option>
-        <option value="">All Category</option>
+        <option value="monitor">All Category</option>
 
       </select>
 
