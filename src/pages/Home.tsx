7a8c8201a3fe92877bbeeb8a39594285e3f25327
rajib5570 <rajib5570@gmail.com>
2024-08-18 13:10:18 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,6 +62,7 @@ const Home: React.FC = () => {
       <select
       className='border border-gray-600 rounded-lg p-2'
       value={selectedBrand}
+      onChange={(e) => setSelectedBrand(e.target.value)}
       >
 
       </select>
