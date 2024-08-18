008625223d7cdb558e309bd59c2bd453a8f0d408
rajib5570 <rajib5570@gmail.com>
2024-08-18 16:51:57 +0600

    home.tsx modified

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_APIKEY,
 	authDomain: import.meta.env.VITE_AUTHDOMAIN,
-	projectId: "bdit-643f8",
+	projectId: ,
 	storageBucket: "bdit-643f8.appspot.com",
 	messagingSenderId: "922799030818",
 	appId: "1:922799030818:web:786e733f16d348da6d007b"
