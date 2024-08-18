a33b61ff3414ae1a7ecf383301b27cf34c90f7de
rajib5570 <rajib5570@gmail.com>
2024-08-18 13:05:09 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -59,6 +59,10 @@ const Home: React.FC = () => {
 
     <div className='flex justify-between items-center m-6'>
 
+      <select>
+        
+      </select>
+
     </div>
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-6'>
