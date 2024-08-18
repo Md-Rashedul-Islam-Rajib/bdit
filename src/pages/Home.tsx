86224a00e7c1b50d18758e07d49c7143727355db
rajib5570 <rajib5570@gmail.com>
2024-08-18 15:09:31 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,7 +94,7 @@ const Home: React.FC = () => {
       >
         <option value="">All Category</option>
         <option value="monitor">Monitor</option>
-        <option value="SSD">Monitor</option>
+        <option value="SSD">Solid State Drive</option>
 
       </select>
 
