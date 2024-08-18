032c82ca0e546fcf3a2891d110bee5db1b2fce43
rajib5570 <rajib5570@gmail.com>
2024-08-18 14:39:54 +0600

    home.tsx modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -70,6 +70,7 @@ const Home: React.FC = () => {
         <option value=''>All Brands</option>
         <option value='AMD'>Advanced Micro Device</option>
         <option value='intel'>Intel</option>
+        <option value='intel'>Intel</option>
 
       </select>
 
